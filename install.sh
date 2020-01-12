75717af666b0f581a0478bb6b2117a3a950b52f3
markv58 <markvittes@gmail.com>
2020-01-12 07:52:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@ sudo mkdir /home/pi/cgi-bin
 sudo mkdir /home/pi/.config/autostart
 sudo cp /home/pi/PCM-UDI-Clone/CgiHttpSRV.desktop /home/pi/.config/autostart
 git config core.filemode false
-if [ -f /home/pi/cgi-bin/CloneCtrl1a ]; then
-  cp /home/pi/cgi-bin/CloneCtrl ~/PCM-UDI-Clone/CloneCtrl1a.bak
-  echo "A backup of CloneCtrl1a file has been saved to the PCM-UDI-Clone folder"
-  echo "This may contain valuable information about your cameras"
-  echo "Previous versions remain in the /cgi-bin/ folder but are not used, you may save or delete them"
-fi
+#if [ -f /home/pi/cgi-bin/CloneCtrl1a ]; then
+#  cp /home/pi/cgi-bin/CloneCtrl ~/PCM-UDI-Clone/CloneCtrl1a.bak
+#  echo "A backup of CloneCtrl1a file has been saved to the PCM-UDI-Clone folder"
+#  echo "This may contain valuable information about your cameras"
+#  echo "Previous versions remain in the /cgi-bin/ folder but are not used, you may save or delete them"
+#fi
 
 sudo cp ~/PCM-UDI-Clone/CgiHttpSRV.py ~/cgi-bin/
 sudo cp ~/PCM-UDI-Clone/2tone ~/cgi-bin/
 sudo cp ~/PCM-UDI-Clone/CloneCtrl1a ~/cgi-bin/
+sudo cp ~/PCM-UDI-Clone/CloneCtrl2 ~/cgi-bin
 sudo chmod +x -R /home/pi/cgi-bin
 
 sudo mv /home/pi/cgi-bin/2tone /usr/bin/

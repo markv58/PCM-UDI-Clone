98ee88288f787b2374088f6ef3172af40fd8d89d
markv58 <markvittes@gmail.com>
2020-01-14 19:11:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+if [ ! -d /home/pi/cgi-bin ]; then
 sudo mkdir /home/pi/cgi-bin
+fi
+
+if [ ! -d /home/pi/.config/autostart ]; then
 sudo mkdir /home/pi/.config/autostart
+fi
+
 sudo cp /home/pi/PCM-UDI-Clone/CgiHttpSRV.desktop /home/pi/.config/autostart
+
 git config core.filemode false
-#if [ -f /home/pi/cgi-bin/CloneCtrl1a ]; then
-#  cp /home/pi/cgi-bin/CloneCtrl ~/PCM-UDI-Clone/CloneCtrl1a.bak
-#  echo "A backup of CloneCtrl1a file has been saved to the PCM-UDI-Clone folder"
-#  echo "This may contain valuable information about your cameras"
-#  echo "Previous versions remain in the /cgi-bin/ folder but are not used, you may save or delete them"
-#fi
 
 sudo cp ~/PCM-UDI-Clone/CgiHttpSRV.py ~/cgi-bin/
 sudo cp ~/PCM-UDI-Clone/2tone ~/cgi-bin/
@@ -30,8 +31,8 @@ sleep 2s
 
 chmod +x PFsetup.sh
 chmod +x setupMM.sh
-./PFsetup.sh normal # set up PictureFrame folders
-./setupMM.sh normal # set up MagicMirror2 folders
+sh PFsetup.sh normal # set up PictureFrame folders
+sh setupMM.sh normal # set up MagicMirror2 folders
 chmod 644 PFsetup.sh
 chmod 644 setupMM.sh
 echo "Reboot in 20 seconds"

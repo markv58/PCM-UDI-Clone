72e1ff86e470d2634e1cb483291ea91755691f8c
markv58 <markvittes@gmail.com>
2020-03-01 09:50:07 -0600

    Update setupSounds.sh

--- setupSounds.sh
+++ setupSounds.sh
@@ -1,1 +1,11 @@
 !#/usr/bin/env bash
+
+echo "Setting up sounds"
+
+if [ != -d "/home/pi/Sounds" ]; then
+  mkdir /home/pi/Sounds
+  cp -a /home/pi/Sounds/. /home/pi/PCM-UDI-Clone/Sounds/
+else
+  echo "Sounds are already installed"
+  exit
+fi
